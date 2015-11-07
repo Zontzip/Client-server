7ba5188d08b4a3fecd6811799a59a822658a3f1c
Zontzor <alex.kiernan@mydit.ie>
2015-11-07 16:23:42 +0000

    Added additional comments to client/server

--- intro/daytimeclient.c
+++ intro/daytimeclient.c
@@ -1,29 +1,45 @@
 #include "unp.h"
 
 int main (int argc, char **argv) {
-	int sockfd, n, counter = 0;
-char recvline[MAXLINE + 1];
+
+	int sockfd, n; /* Define our socket file descriptor */
+	char recvline[MAXLINE + 1];
 	struct sockaddr_in servaddr;
 
 	if (argc != 3)
 		err_quit("usage: a.out <IPaddress>");
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	/* Socket function creates an Internet stream and a TCP socket, the function returns a small int 
+	file descriptor that can be used to identify  the socket in all future function calls, such as connect. */
+	/* The IF state contains a call to the socket function, 
+	then assigns the return value to sockfd and then tests the value */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 		err_sys("socket error");
 
+	/* In the next few lines, we fill in an Internet socket address structure (sockaddr_in structure named seraddr) 
+	with the servers IP address and port number. We set the entire structure to 0 using bzero, then set the address family to AF_INET, 
+	set the port number to the second argument passed in and finally set the IP address to the first argument passed. 
+	The IP address and port number must be in specific formats. htons ("host to network short") converts the binary port number and
+	inet_pton ("presentation to numerid") converts the ASCII IP address into the proper format */
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(atoi(argv[2])); /* daytime server port taken from the command line */
 	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+		/* wrapper function checks for error */
 		err_quit("inet_pton error for %s", argv[1]);
 
+	/* The connect function, when applied to a TCP socket, establishes a TCP connection with the server specified by the socket address structure 
+	as our second argument. We must also specify the length of the socket address structure as our third argument. 
+	In unp.h, SA is defined as a generic socket address structure that must be cast as a pointer because socket functions predate ANSI C standard. 
+	 */
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
+	/* We read the servers reply and display the result using fputs. Read needs to be code in a loop as not all the data may be sent in a single segment, 
+	there might be 1 single segment for 26 bytes or 26 segments of 1 byte. We then terminate read when it returns a 0 signalling there is nothing more to read. */
 	while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
-		counter++; /* count the number of loops completed */
 		recvline[n] = 0; /* null terminate */
-		if (fputs(recvline, stdout) == EOF) /* print data (time and date) returned from server */
+		if (fputs(recvline, stdout) == EOF) /* print data (time and date in this case) returned from server */
 			err_sys("fputs error");
 	}
 
@@ -32,5 +48,6 @@ char recvline[MAXLINE + 1];
 
 	printf("counter = %d\n", counter);
 
+	/* Exit the program and close all open descriptors */ 
 	exit(0);
 }
--- intro/daytimeserver.c
+++ intro/daytimeserver.c
@@ -2,25 +2,39 @@
 #include <time.h>
 
 int main(int argc, char **argv) {
-	int 		listenfd, connfd;
+	int listenfd, connfd;
 	struct sockaddr_in servaddr;
-	char		buff[MAXLINE];
-	time_t		ticks;
+	char buff[MAXLINE];
+	time_t ticks;
 
 	if (argc != 2) 
 		err_quit("usage: <Program Name> <Port No.>");
 
+	/* Creation of socket is identical to client */
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+	/* The servers know port is bound to the socket by filling in an Internet socket address structure 
+	and then calling bind */
+	/* The IP address is specified as INADDR_ANY which allows to server to accept a client connection on 
+	any interface. htonl is same as htons but converts hostlong */
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family 		= AF_INET;
-	servaddr.sin_addr.s_addr 	= htonl(INADDR_ANY);
-	servaddr.sin_port 			= htons(atoi(argv[1]));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi(argv[1]));
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
+	/* By calling listen, the socket is converted to a listening socket that incoming connections 
+	from clients are accepted by the  kernel. socket, bind and listen are the three steps for setting up a 
+	listening descriptor for any TCP server */
 	Listen(listenfd, LISTENQ);
 
+	/* When a handshake is complete, accept returns a value that is used as the connection descriptor. 
+	This new descriptor is used for communication with the new client. A new deriptor is returned by accept 
+	for each client that connects to the server. */
+	/* time returns the current date and time (okeanos is 2 hours ahread) in the form of seconds since jan 1, 1970, 
+	ctime converts this int to human-readable string. A carriage return and linefeed are then appended to the string 
+	by snprintf and result is written to the client by write. snprintf also takes the buffer size to prevent overflow */
 	for( ; ; )
 	{
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
@@ -31,6 +45,8 @@ int main(int argc, char **argv) {
 		
 		getchar();
 
+		/* Server closes connection with client. Initiates normal TCP connection sequence where a 
+		FIN is sent in each direction */
 		Close(connfd);
 	}
 }
